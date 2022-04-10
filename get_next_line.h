f494ddd01691740a1370034614d97051a0fac26d
Pongsakorn Hongpaitoon <phongpai@z3t4c4.42bangkok.com>
2022-04-10 14:33:19 +0700

    k

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phongpai <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/10 10:07:18 by phongpai          #+#    #+#             */
+/*   Updated: 2022/04/10 10:12:05 by phongpai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types>
+#include <sys/uio.h>
+
+#endif
