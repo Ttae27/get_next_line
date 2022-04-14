a1c1f01fab579a6412c0d4fee6069f15a96a1dde
Pongsakorn Hongpaitoon <phongpai@z3t10c2.42bangkok.com>
2022-04-14 17:03:01 +0700

    k

--- testmain.c
+++ testmain.c
@@ -16,7 +16,7 @@ char	*read_to_left(int fd, char *left)
 	if (!buf)
 		return (NULL);
 	rd = 1;
-	while (rd != 1)
+	while (rd != 0)
 	{
 		rd = read(fd, buf, BUFFER_SIZE);
 		if (rd < 0)
@@ -35,7 +35,7 @@ char	*read_to_left(int fd, char *left)
 int	main()
 {
 	int	fd;
-//	char	*line;
+	char	*line;
 	static char	*left;
 
 	fd = open("test1", O_RDONLY);
@@ -44,4 +44,24 @@ int	main()
 		printf("fail");
 	left = read_to_left(fd, left);
 	printf("main left = %s", left);
+	line = get_line(left);
+	left = next_left_str(left);
+	printf("line [1] = %s\n", line);
+	printf("last left = %s\n", left);
+	line = get_line(left);
+	left = next_left_str(left);
+	printf("line [2] = %s\n", line);
+	printf("last left = %s\n", left);	
+	line = get_line(left);
+	left = next_left_str(left);
+	printf("line [3] = %s\n", line);
+	printf("last left = %s\n", left);
+	line = get_line(left);
+	left = next_left_str(left);
+	printf("line [4] = %s\n", line);
+	printf("last left = %s\n", left);
+	line = get_line(left);
+	left = next_left_str(left);
+	printf("line [5] = %s\n", line);
+	printf("last left = %s\n", left);
 }
